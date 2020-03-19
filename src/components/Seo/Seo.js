fb28a42968fc2388b8e1c9d01fbcb73f4637008f
Maja Grubic <maja.grubic@elastic.co>
2020-03-19 22:28:53 +0000

    Trying to fix Twitter image stuff

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -33,7 +33,7 @@ const Seo = props => {
       <meta property="og:type" content="website" />
       <meta property="fb:app_id" content={facebook.appId} />
       {/* Twitter Card tags */}
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content={description} />
       <meta
         name="twitter:creator"
         content={config.authorTwitterAccount ? config.authorTwitterAccount : ""}
